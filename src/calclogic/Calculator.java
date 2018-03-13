76ef15e08c5ebf25d1a6ff7ed887d7fb91ff7f6b
Djossix <stenberg.jessica@gmail.com>
2018-03-13 20:06:01 +0100

    Created test-cases with methods for several expressions.

---
+++ src/calclogic/Calculator.java
@@ -0,0 +1,30 @@
+package calclogic;
+
+public class Calculator {
+
+	public double add(double first, double second) {
+		
+		return first+second;
+	}
+
+	public double sub(double first, double second) {
+		
+		return first-second;
+	}
+
+	public double div(double first, double second) {
+		
+		return first/second;
+	}
+
+	public double multi(double first, double second) {
+		
+		return first*second;
+	}
+
+	public double mod(double first, double second) {
+		
+		return first%second;
+	}
+
+}
